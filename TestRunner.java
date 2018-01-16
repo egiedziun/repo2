a0fe4aacffba8ea60ab8a6c5f6e9e44d8d92933b
egiedziun <egiedziun@gmail.com>
2018-01-16 23:16:50 +0100

    Add files via upload

---
+++ TestRunner.java
@@ -0,0 +1,15 @@
+package com.intive.patronage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.intive.patronage.Tests.SearchGoogle;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SearchGoogle.class})
+//ta klasa musi wiedziec co musi uruchomic
+
+public class TestRunner
+{
+	
+}
